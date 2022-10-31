a5409b7a1a9a7338d96aa26b5a51002f40ddb78c
mejia907 <mejia9070@gmail.com>
2022-10-31 12:22:49 -0500

    Se crea el modulo de categorias en la parte del FrontEnd

--- frontend-games/src/app/app.component.ts
+++ frontend-games/src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Categoria } from './models/categoria';
+import { CategoriaService } from './modules/categoria/services/categoria.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports:[RouterModule, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CategoriaService]
 })
-export class AppComponent {
-  title = 'frontend-games';
+
+export class AppComponent implements OnInit{
+  
+  public categorias: Categoria[] = [];
+  
+  constructor(
+    private categoriaService: CategoriaService
+  ){}
+
+  ngOnInit():void{
+    this.getAllCategorias();
+  }
+
+  getAllCategorias(){
+    this.categoriaService.getAllCategorias().subscribe((data: Categoria[]) =>{
+      this.categorias = data;
+
+    })
+  }
 }
